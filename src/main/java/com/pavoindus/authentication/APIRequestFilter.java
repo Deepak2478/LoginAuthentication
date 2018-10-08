563a500acca9772bb671668997b01d95714b46b0
Pavo Indus <ece1.14101@gmail.com>
2018-10-08 05:48:27 -0700

    initial commit

---
+++ src/main/java/com/pavoindus/authentication/APIRequestFilter.java
@@ -0,0 +1,73 @@
+package com.pavoindus.authentication;
+
+import com.pavoindus.authentication.context.ApplicationContext;
+import com.pavoindus.authentication.model.ApiKey;
+import com.pavoindus.authentication.repository.ApiKeyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class APIRequestFilter implements Filter {
+
+    private static final String PAYROLL_RESULTS_API_KEY_HEADER = "X-Payroll-Results-API-Key";
+    private static final String API_KEY_HEADER = "X-Payroll-Results-Auth-API-Key";
+    private static final String APPLICATION_NAME_HEADER = "X-Payroll-Results-Application";
+    @Autowired
+    private ApiKeyRepository apiKeyRepository;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        if(request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            String origin = request.getHeader("ORIGIN");
+            String headers = request.getHeader("Access-Control-Request-Headers");
+            String methods = request.getHeader("Access-Control-Request-Method");
+            response.setHeader("Access-Control-Allow-Origin", origin);
+            response.setHeader("Access-Control-Allow-Methods", methods);
+            response.setHeader("Access-Control-Allow-Headers", headers);
+            response.setHeader("Access-Control-Max-Age", "1728000");
+            return;
+
+        }
+        String apiKey;
+        String serviceSpecificApiKey = request.getHeader(API_KEY_HEADER);
+        if(serviceSpecificApiKey == null) {
+            apiKey = request.getHeader(PAYROLL_RESULTS_API_KEY_HEADER);
+        } else {
+            apiKey = serviceSpecificApiKey;
+        }
+        String applicationName = request.getHeader(APPLICATION_NAME_HEADER);
+        ApiKey apiKeyObject = null;
+        if(apiKey != null && applicationName != null) {
+            apiKeyObject = apiKeyRepository.findByApiKeyAndApplicationName(apiKey, applicationName);
+        }
+        if(apiKeyObject != null) {
+            ApplicationContext.create();
+            ApplicationContext.get().setApiKey(apiKeyObject);
+            HttpServletResponse response = ((HttpServletResponse) servletResponse);
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            filterChain.doFilter(servletRequest, servletResponse);
+            ApplicationContext.destroy();
+        } else {
+            HttpServletResponse response = ((HttpServletResponse) servletResponse);
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized request");
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
